348497723ec817f0279c75751646ba6367f99022
İzzet Emre Demir <izzetemredemir@gmail.com>
2023-11-18 12:40:57 +0300

    subgraph add

---
+++ subgraph/tests/certificate-utils.ts
@@ -0,0 +1,153 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import {
+  ApprovalForAll,
+  Mint,
+  TransferBatch,
+  TransferSingle,
+  URI,
+  Whitelisted
+} from "../generated/Certificate/Certificate"
+
+export function createApprovalForAllEvent(
+  account: Address,
+  operator: Address,
+  approved: boolean
+): ApprovalForAll {
+  let approvalForAllEvent = changetype<ApprovalForAll>(newMockEvent())
+
+  approvalForAllEvent.parameters = new Array()
+
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  approvalForAllEvent.parameters.push(
+    new ethereum.EventParam("approved", ethereum.Value.fromBoolean(approved))
+  )
+
+  return approvalForAllEvent
+}
+
+export function createMintEvent(
+  owner: Address,
+  carbon: BigInt,
+  cerfId: BigInt
+): Mint {
+  let mintEvent = changetype<Mint>(newMockEvent())
+
+  mintEvent.parameters = new Array()
+
+  mintEvent.parameters.push(
+    new ethereum.EventParam("owner", ethereum.Value.fromAddress(owner))
+  )
+  mintEvent.parameters.push(
+    new ethereum.EventParam("carbon", ethereum.Value.fromUnsignedBigInt(carbon))
+  )
+  mintEvent.parameters.push(
+    new ethereum.EventParam("cerfId", ethereum.Value.fromUnsignedBigInt(cerfId))
+  )
+
+  return mintEvent
+}
+
+export function createTransferBatchEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  ids: Array<BigInt>,
+  values: Array<BigInt>
+): TransferBatch {
+  let transferBatchEvent = changetype<TransferBatch>(newMockEvent())
+
+  transferBatchEvent.parameters = new Array()
+
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam("ids", ethereum.Value.fromUnsignedBigIntArray(ids))
+  )
+  transferBatchEvent.parameters.push(
+    new ethereum.EventParam(
+      "values",
+      ethereum.Value.fromUnsignedBigIntArray(values)
+    )
+  )
+
+  return transferBatchEvent
+}
+
+export function createTransferSingleEvent(
+  operator: Address,
+  from: Address,
+  to: Address,
+  id: BigInt,
+  value: BigInt
+): TransferSingle {
+  let transferSingleEvent = changetype<TransferSingle>(newMockEvent())
+
+  transferSingleEvent.parameters = new Array()
+
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("operator", ethereum.Value.fromAddress(operator))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("from", ethereum.Value.fromAddress(from))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("to", ethereum.Value.fromAddress(to))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+  transferSingleEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromUnsignedBigInt(value))
+  )
+
+  return transferSingleEvent
+}
+
+export function createURIEvent(value: string, id: BigInt): URI {
+  let uriEvent = changetype<URI>(newMockEvent())
+
+  uriEvent.parameters = new Array()
+
+  uriEvent.parameters.push(
+    new ethereum.EventParam("value", ethereum.Value.fromString(value))
+  )
+  uriEvent.parameters.push(
+    new ethereum.EventParam("id", ethereum.Value.fromUnsignedBigInt(id))
+  )
+
+  return uriEvent
+}
+
+export function createWhitelistedEvent(
+  account: Address,
+  isWhitelisted: boolean
+): Whitelisted {
+  let whitelistedEvent = changetype<Whitelisted>(newMockEvent())
+
+  whitelistedEvent.parameters = new Array()
+
+  whitelistedEvent.parameters.push(
+    new ethereum.EventParam("account", ethereum.Value.fromAddress(account))
+  )
+  whitelistedEvent.parameters.push(
+    new ethereum.EventParam(
+      "isWhitelisted",
+      ethereum.Value.fromBoolean(isWhitelisted)
+    )
+  )
+
+  return whitelistedEvent
+}
