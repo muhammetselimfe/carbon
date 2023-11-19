336b7a2016a32b4bafafb2a45781773fe9f2356c
karasakalmt <karasakal99@gmail.com>
2023-11-19 07:07:42 +0300

    frontend connections and bytecode hash change

--- frontend/src/utils/web3/carbonMarket.js
+++ frontend/src/utils/web3/carbonMarket.js
@@ -1,9 +1,8 @@
-import { ethers } from 'ethers';
-import { approval } from './certificate';
+import { ethers } from "ethers";
 
 const provider = new ethers.providers.Web3Provider(window.ethereum);
-const address = '0x86E70857B06f6f5dC441Ef53bE975c88dD8A36a7';
-const fracAbi = ['function fractionizeNft(uint256 nftId, uint256 amount)'];
+const address = "0x86E70857B06f6f5dC441Ef53bE975c88dD8A36a7";
+const fracAbi = ["function fractionizeNft(uint256 nftId, uint256 amount)"];
 
 export const fractionizeNft = async (_id) => {
   //await approval(address);
@@ -12,10 +11,8 @@ export const fractionizeNft = async (_id) => {
   const tx = await contract.fractionizeNft(_id, 1);
 
   const receipt = await tx.wait();
-  console.log('receipt', receipt);
+  console.log("receipt", receipt);
 };
-
-const getNFTAbi = ['function getNftList() view returns (uint256[])'];
 
 export const getNftList = async () => {
   const query = `
@@ -29,10 +26,10 @@ export const getNftList = async () => {
           }
       `;
 
-  const url = 'https://api.studio.thegraph.com/query/57070/avatar/v3/';
+  const url = "https://api.studio.thegraph.com/query/57070/avatar/v3/";
   const options = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query }),
   };
 
@@ -41,20 +38,20 @@ export const getNftList = async () => {
     const data = await response.json();
     return data.data.mints;
   } catch (error) {
-    console.error('Error fetching NFT data:', error);
+    console.error("Error fetching NFT data:", error);
     return null;
   }
 };
 
-const retireNftabi = ['function retireNft(uint256 nftId) payable'];
+const retireNftabi = ["function retireNft(uint256 nftId) payable"];
 
 export const retireNft = async (nftId) => {
   const signer = provider.getSigner();
-  const options = { value: ethers.utils.parseEther('0') };
+  const options = { value: ethers.utils.parseEther("0") };
   const contract = new ethers.Contract(address, retireNftabi, signer);
   console.log(nftId);
   const tx = await contract.functions.retireNft(nftId, options);
 
   const receipt = await tx.wait();
-  console.log('receipt', receipt);
+  console.log("receipt", receipt);
 };
